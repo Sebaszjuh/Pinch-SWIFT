e44702229bde65612d100e30667aeec154d2785e
griends1 <sgriendt@gmail.com>
2021-12-28 17:25:15 -0800

    Implemented coordinates for pinch

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -27,6 +27,8 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
+                
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -67,6 +69,12 @@ struct ContentView: View {
             .onAppear(perform: {
                 isAnimating = true;
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top)
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
